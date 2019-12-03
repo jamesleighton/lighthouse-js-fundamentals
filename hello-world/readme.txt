9753a70dd64040b7fbc8df21a8dad1cff21ba8bf
jamesleighton <mail7788897887@gmail.com>
2019-12-03 04:19:11 +0000

    Use a while loop to repeat a chorus line

---
+++ hello-world/readme.txt
@@ -0,0 +1,3 @@
+hi there. im a new file
+thanks for looking!
+this is working out well.
---
+++ javascripting/for-loop.js
@@ -0,0 +1,7 @@
+let total = 0;
+
+let limit = 45;
+for (i = 0; total < limit; i++) { 
+  total += i;
+}
+console.log(total);
